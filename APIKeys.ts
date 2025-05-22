b128328fa044bb3a74760ef0ec446373d705a186
Luis Rimando <alfonsorimando15@gmail.com>
2025-05-23 02:03:51 +0800

    FIREBASE GUTS

---
+++ APIKeys.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBO9-TgH6ld8C7jqsB-Lk_iHIdpT-10ndA",
+  authDomain: "labwatch-app.firebaseapp.com",
+  projectId: "labwatch-app",
+  storageBucket: "labwatch-app.firebasestorage.app",
+  messagingSenderId: "509091695",
+  appId: "1:509091695:web:76d775ea8617b93854129a",
+  measurementId: "G-8Q35YJN6BB"
+};
---
+++ FirebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeApp } from "firebase/app";
+import { getReactNativePersistence, initializeAuth } from 'firebase/auth';
+import { firebaseConfig } from "./APIKeys";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
